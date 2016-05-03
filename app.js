c36ba4d46c3f45358ebb630822854093c6b047b0
Kacy Hulme (kacyh) <kacyhulme@gmail.com>
2016-05-02 19:10:39 -0500

    move json into sep file part 2

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 (function() {
   var app = angular.module('farmSanctuary', []);
 
-  app.controller('FarmController', function(){
-    this.animals = cows;
-  });
+  app.controller('FarmController', ['$http', function($http){
+    var farm = this;
+    farm.animals = [];
+
+    $http.get('/animals.json').success(function(data) {
+        console.log(data);
+        farm.animals = data;
+    });
+  }]);
 
   app.controller('AdoptionController', function() {
   	this.firstName = "firstName";
@@ -29,158 +35,6 @@
   		this.comment = {};
   	}
   });
-
-  var cows = [ 
-  {
-    name: 'Henry Coolidge',
-    age: 4,
-    color: 'brown',
-    origin: 'New Jersey',
-    history: 'Henry was first from.... ',
-    description: 'Henry has a great personality... ',
-    schedule: 'Henry eats a lot, starting at.... ',
-    canAdopt: true,
-    newAddition: true,
-    images: [
-    {
-    	full: '/images/cow13.jpg',
-    	thumb: '/images/cow14.jpg'
-    },
-    {
-    	full: '/images/cow15.jpg',
-    	thumb: '/images/cow11.jpg'
-    }
-    ],
-    comments: [
-    {
-    	emotion:'Happy',
-    	text: 'We love Henry!',
-    	author: "Megan Smith"
-    },
-    {
-    	emotion: 'Super Happy',
-    	text: 'Henry is adorable',
-    	author: 'Sarah Marks'
-    },
-    {
-    	emotion: 'Happy',
-    	text: 'We want to adopt Henry!',
-    	author: 'Ralph Miller'
-    }
-    ]
-  },
-  {
-    name: 'Molly Moo',
-    age: 2,
-    color: 'brown and black',
-    origin: 'New Hampshire',
-    history: 'Molly Moo was first from.... ',
-    description: 'Molly Moo has a great personality... ',
-    schedule: 'Molly Moo eats a lot, starting at.... ',
-    newAddition: true,
-	images: [
-    {
-    	full: '/images/cow1.jpg',
-    	thumb: '/images/cow2.jpg'
-    },
-    {
-    	full: '/images/cow3.jpg',
-    	thumb: '/images/cow4.jpg'
-    }
-    ],
-    comments: [
-    {
-    	emotion:'Happy',
-    	text: 'We love Henry!',
-    	author: "Megan Smith"
-    },
-    {
-    	emotion: 'Super Happy',
-    	text: 'Henry is adorable',
-    	author: 'Sarah Marks'
-    },
-    {
-    	emotion: 'Happy',
-    	text: 'We want to adopt Henry!',
-    	author: 'Ralph Miller'
-    }
-    ]    
-  },
-  {
-    name: 'Phillip Simon',
-    age: 5,
-    color: 'brown and white',
-    origin: 'Colorado',
-    history: 'Phillip Simon was first from.... ',
-    description: 'Phillip Simon has a great personality... ',
-    schedule: 'Phillip Simon eats a lot, starting at.... ',
-    newAddition: true,
-    images: [
-    {
-    	full: '/images/cow5.jpg',
-    	thumb: '/images/cow6.jpg'
-    },
-    {
-    	full: '/images/cow7.jpg',
-    	thumb: '/images/cow8.jpg'
-    }
-    ],
-    comments: [
-    {
-    	emotion:'Happy',
-    	text: 'We love Henry!',
-    	author: "Megan Smith"
-    },
-    {
-    	emotion: 'Super Happy',
-    	text: 'Henry is adorable',
-    	author: 'Sarah Marks'
-    },
-    {
-    	emotion: 'Happy',
-    	text: 'We want to adopt Henry!',
-    	author: 'Ralph Miller'
-    }
-    ]
-  },
-  {
-    name: 'Cindy Sally',
-    age: 6,
-    color: 'brown',
-    origin: 'Montana',
-    history: 'Cindy Sally was first from.... ',
-    description: 'Cindy Sally has a great personality... ',
-    schedule: 'Cindy Sally eats a lot, starting at.... ',
-    newAddition: true,
-    images: [
-    {
-    	full: '/images/cow9.jpg',
-    	thumb: '/images/cow10.jpg'
-    },
-    {
-    	full: '/images/cow11.jpg',
-    	thumb: '/images/cow12.jpg'
-    }
-    ],
-    comments: [
-    {
-    	emotion:'Happy',
-    	text: 'We love Henry!',
-    	author: "Megan Smith"
-    },
-    {
-    	emotion: 'Super Happy',
-    	text: 'Henry is adorable',
-    	author: 'Sarah Marks'
-    },
-    {
-    	emotion: 'Happy',
-    	text: 'We want to adopt Henry!',
-    	author: 'Ralph Miller'
-    }
-    ]
-  }      
-  ]
 })();
 
 
